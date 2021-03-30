d8a55dfbd51c05d9c403e50b05c37762c08bbea9
Hafsee <haliluhafsat@gmail.com>
2021-03-30 00:24:00 -0600

    Unable to install nodemon

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,5 +1,16 @@
+// jshint esversion:6
 const express = require("express");
 
 const app = express();
 
-app.listen(3000);
+app.get("/", function (request, response){
+    response.send("Hello");
+});
+
+app.get("/About", function (request, response){
+    response.send("Hi i am Hafsat");
+});
+
+app.listen(3000, function () {
+    console.log ("Server started on port 3000");
+});
